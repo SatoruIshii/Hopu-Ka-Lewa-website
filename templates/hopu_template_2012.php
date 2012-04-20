bdb303006b9128b8fae8265dc58dd786ab071a9f
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2012-04-19 23:25:23 -1000

    Use javascript to write head graphic onto all 2012 pages. [nm]

--- templates/hopu_template_2012.php
+++ templates/hopu_template_2012.php
@@ -2,4 +2,16 @@
 {% block head %}
     {{ parent() }}
     <link href='http://fonts.googleapis.com/css?family=Nosifer' rel='stylesheet' type='text/css'>
+    <style type="text/css">
+        #content { background: url(images/Hopucalypse-foot.png) bottom center no-repeat;
+        }
+    </style>
 {% endblock %}
+{% block subfootscript %}
+    {{ parent() }}
+    <script>
+        $(document).ready(function(){
+            $('header>nav').after('<div id="headgraphic" style="position:absolute; opacity:.85; z-index: 10;"><img src="images/Hopucalypse-head.png"/></div>')
+        });
+    </script>
+{% endblock %}    
