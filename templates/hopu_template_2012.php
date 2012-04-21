9c7a6057a8212978c03a77682ee41b194e65f9cb
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2012-04-20 21:18:55 -1000

    Bonehead JS comments with wrong direction slashes. [nm]

--- templates/hopu_template_2012.php
+++ templates/hopu_template_2012.php
@@ -11,10 +11,10 @@
     {{ parent() }}
     <script>
         $(document).ready(function(){
-            \\ Make sure there's enough height to fit the graphics.
+            // Make sure there's enough height to fit the graphics.
             $('#content').css('min-height','600px');
-            \\ Insert top graphic with inline style.
-            $('header>nav').after('<div id="headgraphic" style="position:absolute; opacity:.85; z-index: 10;"><img src="images/Hopucalypse-head.png"/></div>');
+            // Insert top graphic with inline style.
+            $('header>nav').after('<div id="headgraphic" style="position:absolute; opacity:.85; z-index: 10; padding-top:16px;"><img src="images/Hopucalypse-head.png"/></div>');
         });
     </script>
 {% endblock %}    
