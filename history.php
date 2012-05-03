fefd325d45ef8d535d4d4277041eeb258aeb3e82
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2012-05-03 01:04:55 -1000

    Enable cache for test deployment. [nm]

--- history.php
+++ history.php
@@ -3,7 +3,7 @@ require_once 'twig/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
-//  'cache' => 'tmp/chache',
+  'cache' => 'tmp/chache',
 ));
 $template = $twig->loadTemplate('hopu_template_history.php');
 $params = array(
