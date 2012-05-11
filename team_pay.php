aff1580a6d5d5c10906e0be8183b08e6f9ec4368
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2012-05-11 08:32:17 -1000

    First draft Team pay page. [nm]

---
+++ team_pay.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+//  'cache' => 'tmp/chache',
+));
+$template = $twig->loadTemplate('hopu_template_pay.php');
+$params = array(
+  'pagetitle' => 'Pay Team Bid',
+  'description' => "",
+  'keywords' => ''
+  );
+$template->display($params);
+?>
