e83ca96ede8243aa2fc0f37b46708b622a963b84
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2012-05-12 21:57:45 -1000

    Compact stylesheets. Ref #6.
    Minifying and including flexslider.css and style.css into the main
    style-min-XXXXXX.css causes list the list icons in the Flexslider to
    appear vertically instead of where the belong along the bottom of the
    slideshow.
    As such, only roll in normalize.css, grid.css, and superfish.css into
    the main minified css file.

--- templates/hopu_template_general.php
+++ templates/hopu_template_general.php
@@ -11,11 +11,9 @@
     <meta name="description" content="{{ description }}">
     <meta name="keywords" content="{{ keywords }}">
     <!-- CSS-->
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/grid.css">
-    <link rel="stylesheet" href="css/superfish.css">
+    <link rel="stylesheet" href="css/style-min-120512.css" />
     <link rel="stylesheet" href="css/flexslider.css" />
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css" />
     <link rel="stylesheet" href="css/type/kilogram/stylesheet.css">
     <link rel="stylesheet" href="css/custom.css">
     {% for selector, style in customstyle %}
