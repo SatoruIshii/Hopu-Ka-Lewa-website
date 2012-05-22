97f0be3ea007ba8cad01fbcbbb882d70d9d317a6
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2012-05-22 10:14:57 -1000

    Move dates to their own file. [nm]

---
+++ model/dates.php
@@ -0,0 +1,19 @@
+<?php
+$dates = array(
+  // Team dates:
+  'postmark_deadline' => mktime(23,59,59,8,9,2012),
+  'bid_deadline' => mktime(23,59,59,8,17,2012),
+  'invites_out' => mktime(23,59,59,8,24,2012),
+  'accept_deadline' => mktime(23,59,59,8,31,2012),
+  'team_refund' => mktime(23,59,59,9,28,2012),
+  // Individ dates:
+  'earlybird_start' => mktime(),
+  'earlybird_end' => mktime(),
+  'latereg_start' => mktime(),
+  'latereg_end' => mktime(),
+  'shameonyou_start' => mktime(),
+  'shameonyou_end' => mktime(),
+  'full_player_refund' => mktime(),
+  'partial_player_refund' => mktime()
+);
+?>
