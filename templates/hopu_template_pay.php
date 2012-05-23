b7abfbd82fefed1e6a7dc82355a56006f5da092e
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2012-05-22 18:03:22 -1000

    Online payment cutoff for team bid.
    team_pay now checks if $curdate is before the deadline and either
    renders a PayPal button or a helpful "payment closed" message.

--- templates/hopu_template_pay.php
+++ templates/hopu_template_pay.php
@@ -7,6 +7,7 @@
       <h2>Where are you, you spongy, yellow, delicious bastards?</h2>
   </hgroup>
   <section id="team_pay" class="grid_12">
+    {% if hosted_button_id %}
       To pay the team bid fee, please use PayPal:
       <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
         <input type="hidden" name="cmd" value="_s-xclick">
@@ -14,6 +15,9 @@
         <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
         <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
       </form>
+    {% else %} {% autoescape false %}
+      {{ reg_closed_content }} {% endautoescape %}
+    {% endif %}
   </section>
 </div>
 {% endblock %}
