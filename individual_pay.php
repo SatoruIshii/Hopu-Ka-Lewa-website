05431be8b572bad3681ad91c934134e2585c54bf
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2012-05-22 18:03:22 -1000

    Online payment cutoff for team bid.
    team_pay now checks if $curdate is before the deadline and either
    renders a PayPal button or a helpful "payment closed" message.

---
+++ individual_pay.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+//  'cache' => 'tmp/chache',
+));
+$template = $twig->loadTemplate('hopu_template_pay.php');
+$params = array(
+  'pagetitle' => 'Pay Team Bid',
+  'description' => "",
+  'keywords' => '',
+  'hosted_button_id' => '563799WLT7CQL'
+  );
+$template->display($params);
+?>
