55cc32373bfbfdfad37e7bd6cec475246444a6c4
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2012-05-22 18:03:44 -1000

    Begin individual pay page. [nm]

--- individual_pay.php
+++ individual_pay.php
@@ -6,11 +6,14 @@ $twig = new Twig_Environment($loader, array(
 //  'cache' => 'tmp/chache',
 ));
 $template = $twig->loadTemplate('hopu_template_pay.php');
+date_default_timezone_set('Pacific/Honolulu');
+$curdate = date('m-d-Y');
+require 'model/dates.php';
 $params = array(
-  'pagetitle' => 'Pay Team Bid',
-  'description' => "",
-  'keywords' => '',
-  'hosted_button_id' => '563799WLT7CQL'
+  'pagetitle' => 'Pay Individual Fee',
+  'description' => "Online payment for player registration for Hopu Ka Lewa.",
+  'keywords' => 'Hawaii, ultimate, frisbee, ultimate frisbee, coed, tournament, registration, player, pay, fee',
+  'hosted_button_id' => false
   );
 $template->display($params);
 ?>
