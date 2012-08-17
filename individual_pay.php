ab4c0e07d2babc0a3919abb06383747cc6b33c1e
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2012-08-17 13:44:12 -1000

    Update PayPal button code for individual payment.
    New script reflects the labels we've been using for this year.  Tested
    and works on PayPal side.

--- individual_pay.php
+++ individual_pay.php
@@ -19,24 +19,22 @@ $params = array(
 				<section id="individual_pay" class="grid_12">
 						To pay your individual registration fee online, please use PayPal:
 						<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-							<input type="hidden" name="cmd" value="_s-xclick">
-							<input type="hidden" name="hosted_button_id" value="DZ542S6BLSNEL">
-							<table>
-							<tr><td><input type="hidden" name="on0" value="Hopu Ka Lewa 2012 fees">Hopu Ka Lewa 2012 fees</td></tr><tr><td><select name="os0">
-								<option value="Early Bird fee through September 30th">Early Bird fee through September 30th $120.00 USD</option>
-								<option value="Late fee October 1 to October 28r 30th">Late fee October 1 to October 28r 30th $142.00 USD</option>
-								<option value="Shame on you Fee">Shame on you Fee $159.00 USD</option>
-								<option value="Weekend Guest fee">Weekend Guest fee $70.00 USD</option>
-
-							</select> </td></tr>
-							</table>
-							<input type="hidden" name="currency_code" value="USD">
-							<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                          <input type="hidden" name="cmd" value="_s-xclick">
+                          <input type="hidden" name="hosted_button_id" value="DZ542S6BLSNEL">
+                          <table>
+                          <tr><td><input type="hidden" name="on0" value="Hop Ka Lewa 2012 fees">Hop Ka Lewa 2012 fees</td></tr><tr><td><select name="os0">
+                              <option value="Early Bird fee prior to September 30th">Early Bird fee prior to September 30th $120.00 USD</option>
+                              <option value="Late fee October 1 to October 28r 30th">Late fee October 1 to October 28r 30th $142.00 USD</option>
+                              <option value="Shame on You Fee">Shame on You Fee $159.00 USD</option>
 
 
-							<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-						</form>
-						
+                              <option value="Weekend Guest fee">Weekend Guest fee $70.00 USD</option>
+                          </select> </td></tr>
+                          </table>
+                          <input type="hidden" name="currency_code" value="USD">
+                          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                        </form>
 				</section>
 '
 		);
