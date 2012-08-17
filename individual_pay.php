bd995b6bb34faaf1adc02e0aa2f0c774d12b96d3
Clay McKell on Bellman <kcmckell@gmail.com>
2012-08-17 12:27:16 -1000

    Manually edited form options.
    This code causes a PayPal error.  Waiting on new PayPal code.

--- individual_pay.php
+++ individual_pay.php
@@ -19,21 +19,24 @@ $params = array(
 				<section id="individual_pay" class="grid_12">
 						To pay your individual registration fee online, please use PayPal:
 						<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-								<input type="hidden" name="cmd" value="_s-xclick">
-								<input type="hidden" name="hosted_button_id" value="7S88AZ9YXMTH2">
+							<input type="hidden" name="cmd" value="_s-xclick">
+							<input type="hidden" name="hosted_button_id" value="DZ542S6BLSNEL">
+							<table>
+							<tr><td><input type="hidden" name="on0" value="Hopu Ka Lewa 2012 fees">Hopu Ka Lewa 2012 fees</td></tr><tr><td><select name="os0">
+								<option value="Early Bird fee through September 30th">Early Bird fee through September 30th $120.00 USD</option>
+								<option value="Late fee October 1 to October 28r 30th">Late fee October 1 to October 28r 30th $142.00 USD</option>
+								<option value="Shame on you Fee">Shame on you Fee $159.00 USD</option>
+								<option value="Weekend Guest fee">Weekend Guest fee $70.00 USD</option>
 
-								<table>
-								<tr><td><input type="hidden" name="on0" value=""></td></tr><tr><td><select name="os0">
-										<option value="Early Bird fee prior to September 30th">Early Bird fee prior to September 30th : $120.00 USD - monthly</option>
-										<option value="Regular fee October 1 to October 28r 30th">Regular fee October 1 to October 28r 30th : $142.00 USD - monthly</option>
-										<option value="Late Fee">Late Fee : $159.00 USD - monthly</option>
-										<option value="Weekend Guest Fee">Weekend Guest Fee : $70.00 USD - monthly</option>
-										</select> </td></tr>
-								</table>
-								<input type="hidden" name="currency_code" value="USD">
-								<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-								<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+							</select> </td></tr>
+							</table>
+							<input type="hidden" name="currency_code" value="USD">
+							<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+
+
+							<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 						</form>
+						
 				</section>
 '
 		);
