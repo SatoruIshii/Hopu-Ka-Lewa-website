bf767f8674ab9f2ce9f25a6e7125d5482262816d
Clay McKell on Bellman <kcmckell@gmail.com>
2013-06-17 15:20:22 -1000

    Update dates for 2013.

--- register_hopu.php
+++ register_hopu.php
@@ -8,7 +8,7 @@ $twig = new Twig_Environment($loader, array(
 $template = $twig->loadTemplate('hopu_template_2013.php');
 date_default_timezone_set('Pacific/Honolulu');
 $curdate = date('m-d-Y');
-require 'model/dates.php';
+require_once 'model/dates.php';
 $params = array(
   'googleform' => array(
     'active' => FALSE,
@@ -82,9 +82,9 @@ $params = array(
                     <dl>
                         <dt>' . date('M j, Y',$dates['full_player_refund']) . '</dt><dd>Last day to request a full player&#039s refund (minus $15 processing fee).</dd>
                         <dt>' . date('M j, Y',$dates['indiv_reg_end']) . '</dt><dd>Last day to register for regular player rate: $140 (payment must be made online or postmarked by this date).</dd>
-                        <dt>' . date('M j, Y',$dates['latereg_end']) . '</dt><dd>Last day to register for the Late rate: $142 (payment must be made online or postmarked by this date).</dd>
-                        <dt>' . date('M j, Y',$dates['partial_player_refund']) . '</dt><dd>Last day to request a partial player&#039s refund.<br/>Also, last day to register and pay online for the Shame-on-you rate: $159 (mailed checks will not be accepted after ' . date('M j',$dates['latereg_end']) . ').</dd>
-                        <dt>' . date('M j',$dates['online_payment']+1) . '-' . date('j, Y',$dates['shameonyou_end']) . '</dt><dd>Shame-on-you rate applies: $159 and registration and payment must be done at the fields on ' . date('M j',$dates['shameonyou_end']) . '.  Do not mail anything nor register nor pay online.</dd>
+                        <dt>' . date('M j, Y',$dates['indiv_late_end']) . '</dt><dd>Last day to register for the Late rate: $155 (payment must be made online or postmarked by this date).</dd>
+                        <dt>' . date('M j, Y',$dates['online_payment']) . '</dt><dd>Last day to register and pay online for the Shame-on-you rate: $170 (mailed checks will not be accepted after ' . date('M j',$dates['indiv_late_end']) . ').</dd>
+                        <dt>' . date('M j',$dates['online_payment']+1) . '-' . date('j, Y',$dates['indiv_shame_end']) . '</dt><dd>Shame-on-you rate applies: $170 and registration and payment must be done at the fields on ' . date('M j',$dates['indiv_shame_end']) . '.  Do not mail anything nor register nor pay online.</dd>
                     </dl>
                 </section>
                 <section id="paymentoptions" class="grid_4">
@@ -115,10 +115,7 @@ $params = array(
                     </h2>
                     <p>Did something come up?  We&#039re sorry we won&#039t be seeing you this year.  Please keep the following dates in mind when asking for a refund.</p>
                     <dl>
-                        <dt>' . date('M j, Y',$dates['full_player_refund']) . '</dt><dd>Last day for full player&#039s fee refund (minus a processing fee of $15). To qualify for a full refund, you must notify the Tournament Director, via mail or <a href="mailto:hopukalewa14@gmail.com">email</a>, by ' . date('M j',$dates['full_player_refund']) . '.</dd>
-                        <dt>' . date('M j',$dates['full_player_refund']+1) . ' - ' . date('M j, Y',$dates['partial_player_refund']) . '</dt><dd>Partial player&#039s fee refund period.  Players withdrawing from the tournament can receive a partial refund corresponding to
-                                                        <blockquote>Partial Refund = Paid Registration - $70.</blockquote>
-                                                        </dd>
+                        <dt>' . date('M j, Y',$dates['full_player_refund']) . '</dt><dd>Last day for full player&#039s fee refund (minus a processing fee of $15). To qualify for a full refund, you must notify the Tournament Director, via mail or <a href="mailto:hoputd@hawaiiultimate.com">email</a>, by ' . date('M j',$dates['full_player_refund']) . '.</dd>
                     </dl>
                 </section>
                 <section id="pickups" class="grid_7">
