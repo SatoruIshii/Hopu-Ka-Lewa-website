abc42144af5ae07024eacf19a812ac69faa81ccb
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2014-07-14 16:04:45 -1000

    Add deadline warning message.

--- bid_hopu.php
+++ bid_hopu.php
@@ -24,14 +24,20 @@ $params = array(
 $hgroup = '<hgroup class="grid_12">
     <h1>
         Team Bid Processes
-    </h1>'
-. ($params['googleform']['active'] ? 'You can skip straight to the bid form <a href="#'.$params['googleform']['id'].'">here</a>.  We are also accepting <a href="team_pay.php">team bid payments</a>.  Otherwise, read on!' : '') . 
-'</hgroup>';
+    </h1>';
+if ($params['googleform']['active']) {
+  $hgroup .= 'You can skip straight to the bid form <a href="#'.$params['googleform']['id'].'">here</a>.  ';
+  $hgroup .= 'We are also accepting <a href="team_pay.php">team bid payments</a>.  ';
+  $hgroup .= 'Otherwise, read on!';
+  $hgroup .= '<br/>';
+  $hgroup .= '<strong id="deadline_warning" class="'.(strtotime("+1 week",$_SERVER['REQUEST_TIME'])>=$dates['bid_deadline_online'] ? '' : 'hidden').'">NOTICE: Team bid payments submitted after midnight Hawaii Standard Time on '. $MdY['bid_deadline_online'] .' will be considered late.</strong>';
+} else {
+  $hgroup .= 'Sorry, the online bid period is over.'; 
+}
+$hgroup .= '</hgroup>';
+
 $bodycontent = <<<CONTENT
 <section id="summary" class="grid_12">
-    <p>
-      
-    </p>
     <p>
         Hopu Ka Lewa is a Hawaii Ultimate tournament that focuses on capturing and encouraging the Spirit of the Game, and the Spirit of the Community. 
         As the tournament has continued to grow in both size and spirit, the team bid competition has become quite heated. 
