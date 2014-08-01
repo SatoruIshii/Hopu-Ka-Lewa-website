1cfedc9f9c6a686cc6c4d1fe24847432d21f6631
kcmckell <kcmckell@makaeleele.local>
2014-07-31 19:55:29 -0700

    Convert hat draw page to HEREDOC format.

--- hatdraw.php
+++ hatdraw.php
@@ -9,69 +9,72 @@ date_default_timezone_set('Pacific/Honolulu');
 $curdate = date('m-d-Y');
 require 'model/dates.php';
 $template = $twig->loadTemplate('hopu_template_2014.php');
+$content = <<<CONTENT
+<hgroup class="grid_12">
+    <h1>
+        Post-Hopu Hat Draw
+    </h1>
+</hgroup>
+<section id="hatdraw" class="grid_6">
+<h2>Vitals</h2>
+Every year, we keep the party going on the next weekend on an outer island where life is a bit slower and players get a chance to experience another island.
+<dl>
+    <dt>Who</dt>
+    <dd>
+      Anyone who wants some Big Island ultimate.
+    </dd>
+    <dt>What</dt>
+    <dd>
+      A weekend jam-packed with fun:
+      <p>
+        Friday night dinner and (almost) full moon beach ultimate.
+      </p>
+      <p>
+        Hat draw ultimate games on Saturday and Sunday.
+      </p>
+      <p>
+        Saturday night party with nice dinner and entertainment.  Think fire spinning, and DJ/band/both!  We have the Big Pavilion reserved and it is great for dancing.
+      </p>
+    </dd>
+    <dt>When</dt>
+    <dd>
+      November 16-17, 2013.
+    </dd>
+    <dt>Where</dt>
+    <dd>
+      Kamehameha Park in the town of Kapaau (North Kohala District, Hawaii Island).
+    </dd>
+</dl>
+<p>
+Hat-draw registration will be available August 31.  The regular fee will be $45 between August 31 and October 31.  After this, the fee will be $60.
+</p>
+          <p>
+                  Questions and concerns can be directed toward the <a href="mailto:bigislandstay@gmail.com">Hat Draw Organizer, PJ</a>.
+          </p>
+</section>
+<section id="hatdrawmap" class="grid_6">
+<h2>Maps</h2>
+<h3>Fields</h3>
+<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msid=215686218982924613873.000455b1ba1c743521a81&amp;msa=0&amp;ie=UTF8&amp;t=h&amp;ll=20.240133,-155.801497&amp;spn=0.028186,0.036478&amp;z=14&amp;iwloc=lyrftr:msid:215686218982924613873.000455b1ba1c743521a81,00047f45df01af638c6d1,,,0,-31&amp;output=embed"></iframe><br /><small>View <a href="https://maps.google.com/maps/ms?msid=215686218982924613873.000455b1ba1c743521a81&amp;msa=0&amp;ie=UTF8&amp;t=h&amp;ll=20.240133,-155.801497&amp;spn=0.028186,0.036478&amp;z=14&amp;iwloc=lyrftr:msid:215686218982924613873.000455b1ba1c743521a81,00047f45df01af638c6d1,,,0,-31&amp;source=embed" style="color:#0000FF;text-align:left">Kohala Hawaii</a> in a larger map</small>
+<h3>Camping</h3>
+<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?saddr=Kona+International+Airport,+Kailua,+HI&amp;daddr=Samuel+M.+Spencer+Beach+Park,+Waimea,+HI&amp;hl=en&amp;sll=20.128305,-155.810974&amp;sspn=0.413257,0.683899&amp;geocode=FcIoLQEdmgWz9iE3WT-7DL8YqynT5_8BfQ1UeTE3WT-7DL8Yqw%3BFSqIMQEdDli29iHuesZhwFf43SnlY50QxYBTeTHuesZhwFf43Q&amp;oq=spencers&amp;t=h&amp;mra=ls&amp;ie=UTF8&amp;ll=19.8791,-155.926208&amp;spn=0.45201,0.583649&amp;z=10&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?saddr=Kona+International+Airport,+Kailua,+HI&amp;daddr=Samuel+M.+Spencer+Beach+Park,+Waimea,+HI&amp;hl=en&amp;sll=20.128305,-155.810974&amp;sspn=0.413257,0.683899&amp;geocode=FcIoLQEdmgWz9iE3WT-7DL8YqynT5_8BfQ1UeTE3WT-7DL8Yqw%3BFSqIMQEdDli29iHuesZhwFf43SnlY50QxYBTeTHuesZhwFf43Q&amp;oq=spencers&amp;t=h&amp;mra=ls&amp;ie=UTF8&amp;ll=19.8791,-155.926208&amp;spn=0.45201,0.583649&amp;z=10&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+</section>
+<section id="hatdrawlogistics" class="grid_6">
+<h2>Logistics</h2>
+<p>
+At the campground, security comes by at 9pm to check for our safety and then locks the main gate so no leaving or arriving after 9pm.
+</p>
+<p>
+Players should fly into Kona airport (KOA).
+</p>
+
+</section>
+CONTENT;
 $params = array(
   'pagetitle' => 'Hat Draw',
   'description' => 'Outer island hat draw tournament.',
   'keywords' => 'Hawaii, ultimate, outer island, neighbor island, frisbee, ultimate frisbee, coed, hat, hat draw',
-  'content' => '<hgroup class="grid_12">
-                  <h1>
-                      Post-Hopu Hat Draw
-                  </h1>
-              </hgroup>
-              <section id="hatdraw" class="grid_6">
-              <h2>Vitals</h2>
-              Every year, we keep the party going on the next weekend on an outer island where life is a bit slower and players get a chance to experience another island.
-            <dl>
-              <dt>Who</dt>
-              <dd>
-                Anyone who wants some Big Island ultimate.
-              </dd>
-              <dt>What</dt>
-              <dd>
-                A weekend jam-packed with fun:
-                <p>
-                  Friday night dinner and (almost) full moon beach ultimate.
-                </p>
-                <p>
-                  Hat draw ultimate games on Saturday and Sunday.
-                </p>
-                <p>
-                  Saturday night party with nice dinner and entertainment.  Think fire spinning, and DJ/band/both!  We have the Big Pavilion reserved and it is great for dancing.
-                </p>
-              </dd>
-              <dt>When</dt>
-              <dd>
-                November 16-17, 2013.
-              </dd>
-              <dt>Where</dt>
-              <dd>
-                Kamehameha Park in the town of Kapaau (North Kohala District, Hawaii Island).
-              </dd>
-            </dl>
-            <p>
-              Hat-draw registration will be available August 31.  The regular fee will be $45 between August 31 and October 31.  After this, the fee will be $60.
-            </p>
-			<p>
-				Questions and concerns can be directed toward the <a href="mailto:bigislandstay@gmail.com">Hat Draw Organizer, PJ</a>.
-			</p>
-          </section>
-          <section id="hatdrawmap" class="grid_6">
-            <h2>Maps</h2>
-            <h3>Fields</h3>
-            <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msid=215686218982924613873.000455b1ba1c743521a81&amp;msa=0&amp;ie=UTF8&amp;t=h&amp;ll=20.240133,-155.801497&amp;spn=0.028186,0.036478&amp;z=14&amp;iwloc=lyrftr:msid:215686218982924613873.000455b1ba1c743521a81,00047f45df01af638c6d1,,,0,-31&amp;output=embed"></iframe><br /><small>View <a href="https://maps.google.com/maps/ms?msid=215686218982924613873.000455b1ba1c743521a81&amp;msa=0&amp;ie=UTF8&amp;t=h&amp;ll=20.240133,-155.801497&amp;spn=0.028186,0.036478&amp;z=14&amp;iwloc=lyrftr:msid:215686218982924613873.000455b1ba1c743521a81,00047f45df01af638c6d1,,,0,-31&amp;source=embed" style="color:#0000FF;text-align:left">Kohala Hawaii</a> in a larger map</small>
-            <h3>Camping</h3>
-            <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?saddr=Kona+International+Airport,+Kailua,+HI&amp;daddr=Samuel+M.+Spencer+Beach+Park,+Waimea,+HI&amp;hl=en&amp;sll=20.128305,-155.810974&amp;sspn=0.413257,0.683899&amp;geocode=FcIoLQEdmgWz9iE3WT-7DL8YqynT5_8BfQ1UeTE3WT-7DL8Yqw%3BFSqIMQEdDli29iHuesZhwFf43SnlY50QxYBTeTHuesZhwFf43Q&amp;oq=spencers&amp;t=h&amp;mra=ls&amp;ie=UTF8&amp;ll=19.8791,-155.926208&amp;spn=0.45201,0.583649&amp;z=10&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?saddr=Kona+International+Airport,+Kailua,+HI&amp;daddr=Samuel+M.+Spencer+Beach+Park,+Waimea,+HI&amp;hl=en&amp;sll=20.128305,-155.810974&amp;sspn=0.413257,0.683899&amp;geocode=FcIoLQEdmgWz9iE3WT-7DL8YqynT5_8BfQ1UeTE3WT-7DL8Yqw%3BFSqIMQEdDli29iHuesZhwFf43SnlY50QxYBTeTHuesZhwFf43Q&amp;oq=spencers&amp;t=h&amp;mra=ls&amp;ie=UTF8&amp;ll=19.8791,-155.926208&amp;spn=0.45201,0.583649&amp;z=10&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small>
-          </section>
-          <section id="hatdrawlogistics" class="grid_6">
-            <h2>Logistics</h2>
-            <p>
-              At the campground, security comes by at 9pm to check for our safety and then locks the main gate so no leaving or arriving after 9pm.
-            </p>
-            <p>
-              Players should fly into Kona airport (KOA).
-            </p>
-            
-          </section>',
+  'content' => $content,
   'slider' => array(
   array(
       'uri' => 'images/hatdraw/kamehamehastatue.jpg'
